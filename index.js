f8e5114564f359a6b2f63eead86133642cf178c9
Vishal Maharathy <vishalmaharathy76@gmail.com>
2022-08-16 17:23:20 +0530

    final touchups done, flash messages remaining

--- index.js
+++ index.js
@@ -4,11 +4,22 @@ const path = require('path');
 const bcrypt = require('bcrypt');
 require('dotenv').config();
 const connectDB = require('./config/mongoose');
-const nodeMailer = require('./config/nodemailer');
+const sassMiddleware = require('node-sass-middleware');
 
 app.use(express.urlencoded({ extended: false }));
 app.set('view-engine', 'ejs');
 app.use(express.json());
+
+// setup for css files
+app.use(sassMiddleware({
+    /* Options */
+    src: path.join(__dirname, 'assets/scss'),
+    dest: path.join(__dirname, 'assets/css'),
+    debug: true,
+    outputStyle: 'compressed',
+    prefix:'/css'
+}));
+app.use(express.static('assets'));
 
 // connect to mongoose server
 connectDB();
